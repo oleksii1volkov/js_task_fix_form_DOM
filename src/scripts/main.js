b9586895c9d459a6bb432c778cb6786d2f2ac3c8
Oleksii <oleksii.t.volkov@gmail.com>
2025-06-22 02:04:23 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
 // write code here
+const inputs = document.getElementsByTagName('input');
+
+for (const input of inputs) {
+  const labelText = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.htmlFor = input.id;
+  label.textContent = labelText;
+
+  input.placeholder = labelText;
+
+  input.parentNode.insertBefore(label, input);
+}
